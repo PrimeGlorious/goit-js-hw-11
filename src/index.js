c6f4a003a24925db56a0accae1509f58739f6cc9
PrimeGlorious <TheComboPunk@gmail.com>
2023-06-03 01:29:51 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getRefs } from "./js/getRefs";
 import { fetchPictures } from './js/fetchPictures';
 import Notiflix from 'notiflix';
-import { renderMarkup } from "./js/createMarkup";
 import { getRefs } from "./js/getRefs";
 
 const refs = getRefs();
+const lightbox = new SimpleLightbox('.gallery a');
 const options = {
   rootMargin: "400px",
   threshold: 0,
@@ -74,4 +77,36 @@ function renderMarkup(data) {
 
 function clearGalleryList() {
   refs.gallery.innerHTML = '';
+}
+
+function createCardMarkup(arr) {
+  return arr.map(({ likes, views, comments, downloads, tags, webformatURL, largeImageURL }) => {
+    return `
+    <a href="${largeImageURL}" class="gallery-link">
+      <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" width="400" height="250"/>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes </br>${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views </br>${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments </br>${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads </br>${downloads}</b>
+          </p>
+        </div>
+      </div>
+    </a>
+    `
+  }).join('');
+}
+
+function renderMarkup(data) {
+  const markup = createCardMarkup(data);
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
